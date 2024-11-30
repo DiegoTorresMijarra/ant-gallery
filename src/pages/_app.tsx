98c9dd1d25ee0544d4a4ab54971debc727a69ad9
Diego Torres Mijarra <134062570+DiegoTorresMijarra@users.noreply.github.com>
2024-11-30 20:42:36 +0100

    FIx scrollbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,42 @@ import { ConfigProvider } from 'antd'
 import type { AppProps } from 'next/app'
 
 import theme from '../themes/themeConfig'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    * {
+        scrollbar-width: thin;
+        scrollbar-color: #b374f5 #f4f4f4;
+    }
+
+    ::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: linear-gradient(45deg, #b374f5, #669ff5);
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: linear-gradient(45deg, #b374f5, #669ff5);
+    }
+
+    ::-webkit-scrollbar-track {
+        background: #f4f4f4;
+        border-radius: 10px;
+    }
+`
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider theme={theme}>
+    <GlobalStyle />
     <Component {...pageProps} />
   </ConfigProvider>
 )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,38 +2,11 @@ import React from 'react'
 import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import type { DocumentContext } from 'next/document'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-    * {
-        scrollbar-width: thin; /* Tamaño del scrollbar */
-        scrollbar-color: #ff6b6b #f4f4f4;
-    }
-
-    ::-webkit-scrollbar {
-        width: 8px; /* Ancho del scrollbar */
-    }
-
-    ::-webkit-scrollbar-thumb {
-        background: linear-gradient(45deg, #ff6b6b, #f7b731); /* Color con degradado */
-        border-radius: 10px; /* Bordes redondeados */
-    }
-
-    ::-webkit-scrollbar-thumb:hover {
-        background: linear-gradient(45deg, #ff4757, #ffa502); /* Color al pasar el mouse */
-    }
-
-    ::-webkit-scrollbar-track {
-        background: #f4f4f4; /* Color del fondo del scrollbar */
-        border-radius: 10px;
-    }
-`
 
 const MyDocument = () => (
   <Html lang="en">
     <Head />
     <body style={{ margin: 0 }}>
-      <GlobalStyle />
       <Main />
       <NextScript />
     </body>
