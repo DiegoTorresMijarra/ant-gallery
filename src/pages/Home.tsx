3d3f78c275ffcbbe6e7dc0ec32930e0c16719b08
Diego Torres Mijarra <134062570+DiegoTorresMijarra@users.noreply.github.com>
2024-11-30 12:45:14 +0100

    Some changes styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import Gallery from "@/pages/components/gallery";
+import React from 'react'
+import Gallery from '@/pages/components/gallery'
+import { Content, Header } from 'antd/es/layout/layout'
+import Title from 'antd/es/typography/Title'
+import styled from 'styled-components'
 
 const Home = () => (
-    <div className="App">
-        <Gallery />
-    </div>
-);
+  <div className="App">
+    <HeaderEdited>
+      <Title>Jorge y María</Title>
+    </HeaderEdited>
+    <Content>
+      <Gallery />
+    </Content>
+  </div>
+)
 
-export default Home;
+const HeaderEdited = styled(Header)`
+  * {
+    margin: 0;
+  }
+`
+
+export default Home
